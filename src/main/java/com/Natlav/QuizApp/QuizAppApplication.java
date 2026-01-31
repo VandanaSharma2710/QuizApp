e9fcd41d53263f1ed51de9ee4da568918d17b7f9
VandanaSharma2710 <sharmavandana657@gmail.com>
2026-02-01 04:40:12 +0530

    Add .env support for Google OAuth2

--- src/main/java/com/Natlav/QuizApp/QuizAppApplication.java
+++ src/main/java/com/Natlav/QuizApp/QuizAppApplication.java
@@ -1,13 +1,25 @@
 package com.Natlav.QuizApp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class QuizAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuizAppApplication.class, args);
+
+        Dotenv dotenv = Dotenv.load();
+
+        SpringApplication app = new SpringApplication(QuizAppApplication.class);
+        app.setDefaultProperties(Map.of(
+                "GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"),
+                "GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET")
+        ));
+        app.run(args);
+
 	}
 
 }
